acf1219e6ebbe0b5b910115636c6d62d807e24e5
szymon.wojtowicz93 <szymon.wojtowicz93@gmail.com>
2022-05-27 17:36:54 +0200

    add header2 file

---
+++ header2.php
@@ -0,0 +1,17 @@
+<div class = "logo">
+	<div class = "left_logo">
+		<a href="index.php"><img src="src/tropet4.png"></a> 
+	</div>
+	<div class = "mid_logo">
+		<div class ="mid">
+			<h1>Odnajdź Swojego pupila</h1>
+		</div>
+		<div class="menu">
+			<nav>
+			<?php 
+				require_once "menu.php";
+			?>
+			</nav>
+		</div>
+	</div>
+</div>
